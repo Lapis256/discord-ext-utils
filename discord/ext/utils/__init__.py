ea4771062e2b0d5547119082371abf9d6cde74a9
lapis256 <lapis128.256@gmail.com>
2021-01-24 02:21:44 +0900

    __version__を追加

--- discord/ext/utils/__init__.py
+++ discord/ext/utils/__init__.py
@@ -1,2 +1,5 @@
 from .bot import Bot, AutoShardedBot
 from .presence import PresenceTask, presence
+from .expander import *
+
+__version__ = "0.3.0"
--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
+from re import search
+
+with open("discord/ext/utils/__init__.py") as f:
+    version = search(r'__version__\s=\s"(.*?)"', f.read()).group(1)
 
 setup(
     name="discord-ext-utils",
     author="Lapis256",
     url="https://github.com/Lapis256/discord-ext-utils",
-    version="0.3",
+    version=version,
     packages=["discord.ext.utils", "discord.ext.utils.cogs"],
     license="MIT",
     description="An extension module of discord.ext.commands.",
